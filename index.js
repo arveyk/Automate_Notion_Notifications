b4ee2e1ea772095491c3aaea0d10747a92891d9b
arveyk <harveykisiangani@gmail.com>
2025-08-27 17:36:20 +0300

    Package.json files

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-import { PORT } from "./env.js";
-
 import express from "express";
 const APP_PORT = parseInt(PORT) || 8080;
 
-const expressApp = express();
+const router = express.Router();
+router.use(router);
 
-//expressApp.use(express.urlencoded({ extended: false }));
-//expressApp.use(express.json());
-expressApp.use(router);
-
-expressApp.use((request, response, next) => {
+router.use((request, response, next) => {
   console.log(`${Date.now()}`);
   next();
 });
 
-expressApp.listen(APP_PORT, () => {
-  console.log(`Server's ears on port: ${APP_PORT}`);
-});
-
-export { expressApp as app };
+export { router as app };
--- server.js
+++ server.js
@@ -3,19 +3,20 @@ import { PORT } from "./env.js";
 import express from "express";
 const APP_PORT = parseInt(PORT) || 8080;
 
-const expressApp = express();
+const app = express();
 
-//expressApp.use(express.urlencoded({ extended: false }));
-//expressApp.use(express.json());
-expressApp.use(router);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-expressApp.use((request, response, next) => {
+app.use(router);
+
+app.use((request, response, next) => {
   console.log(`${Date.now()}`);
   next();
 });
 
-expressApp.listen(APP_PORT, () => {
+app.listen(APP_PORT, () => {
   console.log(`Server's ears on port: ${APP_PORT}`);
 });
 
-export { expressApp as app };
+export { app };
